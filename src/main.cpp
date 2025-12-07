e83749c7528c53be47712e580bf140db83a19948
bokrosf <bokrosf@owner.com>
2025-12-08 00:20:16 +0100

    SDL initialization and black screen rendering.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,79 @@
 #include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_init.h>
+
+struct app_context
+{
+    bool running;
+    int width;
+    int height;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+};
+
+app_context app = app_context
+{
+    .running = true,
+    .width = 800,
+    .height = 600,
+    .window = nullptr,
+    .renderer = nullptr,
+};
+
+void handle_events();
+void render();
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Text Rendering PoC" << std::endl;
+    const SDL_InitFlags init_flags = SDL_INIT_EVENTS | SDL_INIT_VIDEO;
+    const SDL_WindowFlags window_flags = 0;
+    SDL_Init(init_flags);
+    SDL_CreateWindowAndRenderer(
+        "text-rendering",
+        app.width,
+        app.height,
+        window_flags,
+        &app.window,
+        &app.renderer
+    );
+
+    while (app.running)
+    {
+        handle_events();
+        render();
+    }
+
+    SDL_DestroyRenderer(app.renderer);
+    SDL_DestroyWindow(app.window);
+    SDL_QuitSubSystem(init_flags);
+    SDL_Quit();
 
     return 0;
+}
+
+void handle_events()
+{
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_EVENT_QUIT)
+        {
+            app.running = false;
+            
+            return;
+        }
+        
+        if (event.type == SDL_EVENT_KEY_DOWN)
+        {
+            // Handle keypress: event.key.key == SDLK_A
+        }
+    }
+}
+
+void render()
+{
+    SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, 0);
+    SDL_RenderClear(app.renderer);
+    SDL_RenderPresent(app.renderer);
 }
